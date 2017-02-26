5a27302326641c87dff61b9e0fa6e6b6aadd62b7
Ankur Sundara <me@ankursundara.com>
2017-02-26 11:06:46 -0600

    Create secrets.example.py

---
+++ secrets.example.py
@@ -0,0 +1,7 @@
+INF_APP_ID = 'xxxxxxxx'
+INF_APP_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+CLIENT_ACCESS_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+FIREBASE_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+FIREBASE_AUTH_DOMAIN = ''
+FIREBASE_DB_URL = 'https://your-firebase-url.firebaseio.com/'
+FIREBASE_STORAGE_BUCKET = ''
